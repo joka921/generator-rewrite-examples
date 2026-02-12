a549a7b94cd8785053903023ebbb94ff72b6b11a
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2026-02-12 12:14:50 +0100

    Initial commit. Still working on the infrastructure

---
+++ generator.h
@@ -0,0 +1,234 @@
+#ifndef GENERATOR_REWRITE_EXAMPLES_GENERATOR_H
+#define GENERATOR_REWRITE_EXAMPLES_GENERATOR_H
+
+    // Drop-in replacement for `std::suspend_always` and `std::suspend_never` from C++20.
+    // As we will use them with our own `coroutine_handle` types, the `await_suspend` function
+    // is templated.
+    struct SuspendAlways {
+        static constexpr bool await_ready() const noexcept { return false; }
+
+        template<typename Handle>
+        static constexpr void await_suspend([[maybe_unused]] Handle handle) const noexcept {
+        }
+
+        static constexpr void await_resume() const noexcept {
+        }
+    };
+
+struct SuspendNever {
+    static constexpr bool await_ready() const noexcept { return true; }
+
+    template<typename Handle>
+    static constexpr void await_suspend([[maybe_unused]] Handle handle) const noexcept {
+    }
+
+    static constexpr void await_resume() const noexcept {
+    }
+};
+
+ // A simple generator class. It is templated on the used `Handle` type, so we can either use it with `std::coroutine_handle`, or with our own.
+    template<typename T,
+        template <typename...> typename GeneratorHandle =
+        std::coroutine_handle>
+    class generator;
+
+    namespace detail {
+        template<typename T,
+            template <typename...> typename GeneratorHandle =
+            std::coroutine_handle>
+        class generator_promise {
+        public:
+            // Even if the generator only yields `const` values, the `value_type`
+            // shouldn't be `const` because otherwise several static checks when
+            // interacting with the STL fail.
+            using value_type = std::remove_cvref_t<T>;
+            using reference_type = std::conditional_t<std::is_reference_v<T>, T, T &>;
+            using pointer_type = std::remove_reference_t<T> *;
+
+            generator_promise() = default;
+
+            generator<T, GeneratorHandle> get_return_object() noexcept;
+
+            constexpr cppcoro::SuspendAlways initial_suspend() const noexcept {
+                return {};
+            }
+
+            constexpr cppcoro::SuspendAlways final_suspend() const noexcept { return {}; }
+
+            template<typename U = T,
+                std::enable_if_t<!std::is_rvalue_reference<U>::value, int> = 0>
+            cppcoro::SuspendAlways yield_value(
+                std::remove_reference_t<T> &value) noexcept {
+                m_value = std::addressof(value);
+                return {};
+            }
+
+            cppcoro::SuspendAlways yield_value(
+                std::remove_reference_t<T> &&value) noexcept {
+                m_value = std::addressof(value);
+                return {};
+            }
+
+            void unhandled_exception() { m_exception = std::current_exception(); }
+
+            void return_void() {
+            }
+
+            reference_type value() const noexcept {
+                return static_cast<reference_type>(*m_value);
+            }
+
+            // Don't allow any use of 'co_await' inside the generator coroutine.
+            template<typename U>
+            void await_transform(U &&value) = delete;
+
+            void rethrow_if_exception() const {
+                if (m_exception) {
+                    std::rethrow_exception(m_exception);
+                }
+            }
+
+        private:
+            pointer_type m_value;
+            std::exception_ptr m_exception;
+        };
+
+        struct generator_sentinel {
+        };
+
+        template<typename T,
+            template <typename...> typename GeneratorHandle,
+            bool ConstDummy = false>
+        class generator_iterator {
+            using promise_type = generator_promise<T, GeneratorHandle>;
+            using coroutine_handle = GeneratorHandle<promise_type>;
+
+        public:
+            using iterator_category = std::input_iterator_tag;
+            // What type should we use for counting elements of a potentially infinite
+            // sequence?
+            using difference_type = std::ptrdiff_t;
+            using value_type = typename promise_type::value_type;
+            using reference = typename promise_type::reference_type;
+            using pointer = typename promise_type::pointer_type;
+
+            // Iterator needs to be default-constructible to satisfy the Range concept.
+            generator_iterator() noexcept : m_coroutine(nullptr) {
+            }
+
+            explicit generator_iterator(coroutine_handle coroutine) noexcept
+                : m_coroutine(coroutine) {
+            }
+
+            friend bool operator==(const generator_iterator &it,
+                                   generator_sentinel) noexcept {
+                return !it.m_coroutine || it.m_coroutine.done();
+            }
+
+            friend bool operator!=(const generator_iterator &it,
+                                   generator_sentinel s) noexcept {
+                return !(it == s);
+            }
+
+            friend bool operator==(generator_sentinel s,
+                                   const generator_iterator &it) noexcept {
+                return (it == s);
+            }
+
+            friend bool operator!=(generator_sentinel s,
+                                   const generator_iterator &it) noexcept {
+                return it != s;
+            }
+
+            generator_iterator &operator++() {
+                m_coroutine.resume();
+                if (m_coroutine.done()) {
+                    m_coroutine.promise().rethrow_if_exception();
+                }
+
+                return *this;
+            }
+
+            // Need to provide post-increment operator to implement the 'Range' concept.
+            void operator++(int) { (void) operator++(); }
+
+            reference operator*() const noexcept { return m_coroutine.promise().value(); }
+
+            pointer operator->() const noexcept { return std::addressof(operator*()); }
+
+        private:
+            coroutine_handle m_coroutine;
+        };
+    } // namespace detail
+
+    template<typename T,
+        template <typename...> typename GeneratorHandle>
+    class [[nodiscard]] generator {
+    public:
+        using promise_type = detail::generator_promise<T, GeneratorHandle>;
+        using iterator =
+        detail::generator_iterator<T, GeneratorHandle, false>;
+        using value_type = typename iterator::value_type;
+
+        generator() noexcept : m_coroutine(nullptr) {
+        }
+
+        generator(generator &&other) noexcept : m_coroutine(other.m_coroutine) {
+            other.m_coroutine = nullptr;
+        }
+
+        generator(const generator &other) = delete;
+
+        ~generator() {
+            if (m_coroutine) {
+                m_coroutine.destroy();
+            }
+        }
+
+        generator &operator=(generator other) noexcept {
+            swap(other);
+            return *this;
+        }
+
+        iterator begin() {
+            if (m_coroutine) {
+                m_coroutine.resume();
+                if (m_coroutine.done()) {
+                    m_coroutine.promise().rethrow_if_exception();
+                }
+            }
+
+            return iterator{m_coroutine};
+        }
+
+        detail::generator_sentinel end() noexcept {
+            return detail::generator_sentinel{};
+        }
+
+        void swap(generator &other) noexcept {
+            std::swap(m_coroutine, other.m_coroutine);
+        }
+
+    private:
+        friend class detail::generator_promise<T, GeneratorHandle>;
+
+        GeneratorHandle<promise_type> m_coroutine;
+    };
+
+    template<typename T, template <typename...> typename H>
+    void swap(generator<T, H> &a, generator<T, D, H> &b) {
+        a.swap(b);
+    }
+
+    namespace detail {
+        template<typename T, template <typename...> typename H>
+        generator<T, H>
+        generator_promise<T, H>::get_return_object() noexcept {
+            using coroutine_handle = H<generator_promise<T, H> >;
+            return generator<T, H>{coroutine_handle::from_promise(*this)};
+        }
+    } // namespace detail
+
+
+
+#endif //GENERATOR_REWRITE_EXAMPLES_GENERATOR_H
---
+++ type_traits.h
@@ -0,0 +1,102 @@
+#ifndef GENERATOR_REWRITE_EXAMPLES_TYPE_TRAITS_H
+#define GENERATOR_REWRITE_EXAMPLES_TYPE_TRAITS_H
+
+// Helper type traits used to analyze promise types and awaitables.
+namespace coro_detail
+{
+    // A class that is implicitly convertible to anything, but the conversion operator is not defined. Useful for type traits.
+    struct ConvertibleToAnything {
+        template<typename T>
+        operator T();
+    };
+
+    // `has_await_transform<Promise>::value` is true iff. `Promise` has a member function `await_transform` that can be called
+    // with a single argument.
+    // NOTE: This actually is just a proxy, in reality we would have to check for ANY member function called `await_transform`,
+    // But that is hard to do without reflection, so we leave this proxy for now.
+    template<typename Promise, typename = void>
+    struct has_await_transform : std::false_type {
+    };
+
+    template<typename Promise>
+    struct has_await_transform<Promise,
+                std::void_t<decltype(std::declval<Promise &>()
+                    .await_transform(std::declval<ConvertibleToAnything>()))> >
+            : std::true_type {
+    };
+
+    // Given a `promise` and a `co_await`ed expression, get the actual `awaitable`, possibly through the `await_transform` mechanism.
+    // NOTE: We currently deliberately ignore the possibility of `operator co_await` being present. This might be fixed
+    // Until C++Now.
+    template<typename Promise, typename Expr>
+    decltype(auto) get_awaitable(Promise &promise, Expr &&expr) {
+        // TODO what about co_await <something that is void>)
+        if constexpr (has_await_transform<Promise>::value) {
+            return promise.await_transform(std::forward<Expr>(expr));
+        } else {
+            return std::forward<Expr>(expr);
+        }
+    }
+
+    // Type trait to check whether a given `Promise` has a `return_void` member function.
+    template<typename Promise, typename = void>
+    struct has_return_void : std::false_type {
+    };
+
+    template<typename Promise>
+    struct has_return_void<Promise,
+                std::void_t<decltype(std::declval<Promise &>()
+                    .return_void())> >
+            : std::true_type {
+    };
+
+
+    // Type trait that checks whether a given promise type has an overloaded `operator new`.
+    // Note: This only supports the simple form that only takes a `size_t`.
+    template<typename P, typename = void>
+    struct has_promise_new : std::false_type {
+    };
+
+    template<typename P>
+    struct has_promise_new<P,
+                std::void_t<decltype(P::operator new(size_t{}))> >
+            : std::true_type {
+    };
+
+    // Type trait that checks whether a given promise type has an overloaded `operator new`
+    // that accepts the coroutine arguments in addition to the size.
+    // Per the C++ spec, `promise_type::operator new(size, args...)` is tried first.
+    template<typename P, typename ArgsTuple, typename = void>
+    struct has_promise_new_with_args_impl : std::false_type {};
+
+    template<typename P, typename... Args>
+    struct has_promise_new_with_args_impl<P, std::tuple<Args...>,
+                std::void_t<decltype(P::operator new(size_t{}, std::declval<Args>()...))>>
+            : std::true_type {};
+
+    template<typename P, typename... Args>
+    using has_promise_new_with_args = has_promise_new_with_args_impl<P, std::tuple<Args...>>;
+
+
+    // Type traits that checks whether the promise_type `P` has a member `operator delete`.
+    template<typename P, typename = void>
+    struct has_promise_delete : std::false_type {
+    };
+
+    template<typename P>
+    struct has_promise_delete<P,
+                std::void_t<decltype(P::operator delete(
+                    std::declval<void *>(), size_t{}))> >
+            : std::true_type {
+    };
+
+    // Type trait for unsized promise delete: `P::operator delete(void*)`.
+    template<typename P, typename = void>
+    struct has_promise_delete_unsized : std::false_type {};
+
+    template<typename P>
+    struct has_promise_delete_unsized<P,
+                std::void_t<decltype(P::operator delete(std::declval<void *>()))>>
+            : std::true_type {};
+}
+#endif //GENERATOR_REWRITE_EXAMPLES_TYPE_TRAITS_H
